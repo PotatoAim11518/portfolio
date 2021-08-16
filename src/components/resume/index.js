5304a40f2e023610555cc3db5934d0bf0b128c29
wilson <whuang3015@gmail.com>
2021-08-16 02:46:13 -0700

    create resume component

--- src/components/resume/index.js
+++ src/components/resume/index.js
@@ -1,12 +1,28 @@
-import React from "react";
-import { Document } from "react-pdf";
-import resumePDF from '../../assets/HUANG_WILSON_RESUME.pdf';
+import React, { useState } from "react";
+import { Document, Page } from "react-pdf/dist/esm/entry.webpack";
+import { pdfjs } from "react-pdf";
+import resumePDF from "../../assets/HUANG_WILSON_RESUME.pdf";
+import { AiOutlineDownload } from "react-icons/ai";
+
+import "react-pdf/dist/umd/Page/AnnotationLayer.css";
+import "./resume.css";
 
 const Resume = () => {
+  pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+
   return (
     <>
-      <p>resume</p>
-      <Document file={"./HUANG_WILSON_RESUME.pdf"}></Document>
+      <div id="pdf">
+        <Document file={resumePDF} renderMode="canvas">
+          <Page pageNumber={1} />
+        </Document>
+      </div>
+      <div>
+        <a className={"socialButton"} href="https://drive.google.com/uc?export=download&id=1guI4JiD6wrRyU6BeP1MJEKSVx3SbYK8_">
+          <AiOutlineDownload />
+          <p className={"buttonLabel"}> Download</p>
+        </a>
+      </div>
     </>
   );
 };
