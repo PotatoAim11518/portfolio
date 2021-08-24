74c015bcf97bf18c979a2452717bd3a85a9129ab
wilson <whuang3015@gmail.com>
2021-08-24 16:39:45 -0700

    fix link colors

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-import logo from './logo.svg';
-import './App.css';
 import { BrowserRouter } from 'react-router-dom';
 import Homepage from './components/homepage';
 import NavBar from './components/navbar';
