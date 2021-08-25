bb45944afd9288632dbdb143f3497cddd8a764d3
wilson <whuang3015@gmail.com>
2021-08-24 18:59:38 -0700

    fix projects section height

--- src/components/projects/index.js
+++ src/components/projects/index.js
@@ -11,20 +11,20 @@ const Projects = () => {
   return (
     <div className={styles.projectsWrapper}>
       <ProjectCard
-        imageUrl={potatoExchange}
-        name={"Potato Exchange"}
-        description={"Potato Exchange is a platform for users to be able to host small, personal meet-and-greets with guests."}
-        technologies={"JavaScript, Python, React.js, Redux, Flask, Socket.IO, SLQAlchemy, postgreSQL, HTML, CSS, Heroku"}
-        liveSite={"https://potatoexchange.herokuapp.com/"}
-        gitHub={"https://github.com/PotatoAim11518/potato_exchange"}
-      />
-      <ProjectCard
         imageUrl={catsy}
         name={"Catsy"}
         description={"Catsy is a cat adoption app styled after a combination of Etsy and Petfinder."}
         technologies={"JavaScript, Python, React.js, Redux, Flask, SLQAlchemy, postgreSQL, HTML, CSS, Petfinder API, Heroku"}
         liveSite={"https://catsyapp.herokuapp.com/"}
         gitHub={"https://github.com/commanderh/catsy"}
+      />
+      <ProjectCard
+        imageUrl={potatoExchange}
+        name={"Potato Exchange"}
+        description={"Potato Exchange is a platform for users to be able to host small, personal meet-and-greets with guests."}
+        technologies={"JavaScript, Python, React.js, Redux, Flask, Socket.IO, SLQAlchemy, postgreSQL, HTML, CSS, Heroku"}
+        liveSite={"https://potatoexchange.herokuapp.com/"}
+        gitHub={"https://github.com/PotatoAim11518/potato_exchange"}
       />
       <ProjectCard
         imageUrl={linkshell}
--- src/components/projects/projectCard.js
+++ src/components/projects/projectCard.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { IoIosRocket } from "react-icons/io";
 import { FaGithub } from "react-icons/fa";
 
@@ -12,34 +12,49 @@ const ProjectCard = ({
   liveSite,
   gitHub,
 }) => {
+  const [hiddenInfo, setHiddenInfo] = useState(true);
+
   return (
     <div className={styles.projectCard}>
+      <h1 className={styles.name}>{name}</h1>
       <div
-        className={styles.projectImage}
-        style={{backgroundImage: "url(" + imageUrl + ")"}}
-      ></div>
-      <div className={styles.projectHover}>
-        <h1 className={styles.name}>{name}</h1>
-        <p className={styles.description}>{description}</p>
-        <p className={styles.technologies}>{technologies}</p>
-        <div className={styles.buttonContainer}>
-
-          <div className={styles.button}>
-            <a className={styles.buttonLink} href={liveSite} target="_blank" rel="noopener noreferrer">
-              <IoIosRocket />
-              <p className={styles.buttonLabel}> Live Site</p>
-            </a>
-          </div>
-
-          <div className={styles.button}>
-            <a className={styles.buttonLink} href={gitHub} target="_blank" rel="noopener noreferrer">
-              <FaGithub />
-              <p className={styles.buttonLabel}> GitHub</p>
-            </a>
-          </div>
-
+        className={styles.projectHoverContainer}
+        onMouseLeave={() => setHiddenInfo(true)}
+      >
+        <div
+          className={styles.projectImage}
+          onMouseOver={() => setHiddenInfo(false)}
+          style={{ backgroundImage: "url(" + imageUrl + ")" }}
+        ></div>
+        <div className={styles.projectHover} hidden={hiddenInfo}>
+          <p className={styles.description}>{description}</p>
+          <p className={styles.technologies}>{technologies}</p>
+        </div>
+      </div>
+      <div className={styles.buttonContainer}>
+        <div className={styles.button}>
+          <a
+            className={styles.buttonLink}
+            href={liveSite}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <IoIosRocket />
+            <p className={styles.buttonLabel}> Live Site</p>
+          </a>
         </div>
 
+        <div className={styles.button}>
+          <a
+            className={styles.buttonLink}
+            href={gitHub}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaGithub />
+            <p className={styles.buttonLabel}> GitHub</p>
+          </a>
+        </div>
       </div>
     </div>
   );
